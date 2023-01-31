aa08bfa535e5ad28fa26fb69b27725685753a697
jo <kaitokinjo@gmail.com>
2023-01-31 18:01:57 +0900

    added items

--- app/main.py
+++ app/main.py
@@ -6,3 +6,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}

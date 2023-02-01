ce806aedced96b2af2f786cd63db8bdab9db383b
jo <kaitokinjo@gmail.com>
2023-02-01 11:05:28 +0900

    added routes

--- api/main.py
+++ api/main.py
@@ -1,15 +1,10 @@
 from fastapi import FastAPI
+from routers import users, items
 
 app = FastAPI()
+app.include_router(users.router)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}

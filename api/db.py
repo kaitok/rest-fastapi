fe10606f26c1ed04af08295bf83f2e695e10732f
jo <kaitokinjo@gmail.com>
2023-02-03 16:28:53 +0900

    added db

---
+++ api/db.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+username = os.environ.get("POSTGRES_USER")
+password = os.environ.get("POSTGRES_PASSWORD")
+database = os.environ.get("POSTGRES_DB")
+host = os.environ.get("HOST")
+port = os.environ.get("PORT")
+
+SQLALCHEMY_DATABASE_URL = "postgresql://{username}:{password}@{host}:{port}/{database}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from .routers import users
+from db import SessionLocal
 
 app = FastAPI()
 app.include_router(users.router)
@@ -8,3 +9,11 @@ app.include_router(users.router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()

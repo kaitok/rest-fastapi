6fb221f56b7f9254fa6257369889043d74ed223e
jo <kaitokinjo@gmail.com>
2023-02-04 20:46:11 +0900

    Create profile.py

---
+++ api/models/profile.py
@@ -0,0 +1,12 @@
+from api.db.database import Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship, backref
+from .mixins import TimestampMixin
+
+
+class Profile(Base, TimestampMixin):
+    __tablename__ = "profile"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String(length=100), nullable=False)
+    last_name = Column(String(length=100), nullable=False)

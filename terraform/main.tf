337172acf1557b028c979cfb1b6191d5f5cd2217
jo <kaitokinjo@gmail.com>
2023-02-13 14:11:56 +0900

    added terraform

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_ecr_repository" "api_ecr" {
+  name = "rest-fastapi-repository"
+}

7f9494dc5845d0a84af2d4f06d61078fdf7f15e6
jo <kaitokinjo@gmail.com>
2023-02-16 18:39:39 +0900

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,279 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
+
+# VPC
+resource "aws_vpc" "rest_fastapi" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "rest_fastapi"
+  }
+}
+
+
+# Public Subnet
+resource "aws_subnet" "public_0" {
+  vpc_id                  = aws_vpc.rest_fastapi.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.rest_fastapi.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "rest_fastapi" {
+  vpc_id = aws_vpc.rest_fastapi.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.rest_fastapi.id
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.rest_fastapi.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public_0" {
+  subnet_id      = aws_subnet.public_0.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+# Private Subnet
+resource "aws_subnet" "private_0" {
+  vpc_id                  = aws_vpc.rest_fastapi.id
+  cidr_block              = "10.0.65.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id                  = aws_vpc.rest_fastapi.id
+  cidr_block              = "10.0.66.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_route_table" "private_0" {
+  vpc_id = aws_vpc.rest_fastapi.id
+}
+
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.rest_fastapi.id
+}
+
+resource "aws_route" "private_0" {
+  route_table_id         = aws_route_table.private_0.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_0.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route" "private_1" {
+  route_table_id         = aws_route_table.private_1.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_1.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_eip" "nat_gateway_0" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.rest_fastapi]
+}
+
+resource "aws_eip" "nat_gateway_1" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.rest_fastapi]
+}
+
+resource "aws_nat_gateway" "nat_gateway_0" {
+  allocation_id = aws_eip.nat_gateway_0.id
+  subnet_id     = aws_subnet.public_0.id
+  depends_on    = [aws_internet_gateway.rest_fastapi]
+}
+
+resource "aws_nat_gateway" "nat_gateway_1" {
+  allocation_id = aws_eip.nat_gateway_1.id
+  subnet_id     = aws_subnet.public_1.id
+  depends_on    = [aws_internet_gateway.rest_fastapi]
+}
+
+
+# S3
+resource "aws_s3_bucket" "private" {
+  bucket = "private-rest-fastapi"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket                  = aws_s3_bucket.private.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "alb-log-rest-fastapi"
+
+  lifecycle_rule {
+    enabled = true
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["582318560864"]
+    }
+  }
+}
+
+
+
+# ALB
+resource "aws_lb" "rest_fastapi" {
+  name                       = "rest_fastapi"
+  load_balancer_type         = "application"
+  internal                   = false
+  idle_timeout               = 60
+  enable_deletion_protection = true
+
+  subnets = [
+    aws_subnet.public_0.id,
+    aws_subnet.public_1.id,
+  ]
+
+  access_logs {
+    bucket  = aws_s3_bucket.alb_log.id
+    enabled = true
+  }
+
+  security_groups = [
+    module.http_sg.security_group_id,
+    module.https_sg.security_group_id,
+    module.http_redirect_sg.security_group_id,
+  ]
+}
+
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+}
+
+
+# ECR
 resource "aws_ecr_repository" "api_ecr" {
   name = "rest-fastapi"
 }
+
+
+
+# ECS
+resource "aws_ecs_cluster" "api_ecs" {
+  name = "rest-fastapi"
+}
+
+resource "aws_ecs_task_definition" "api_ecs" {
+  family                   = "rest-fastapi"
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  container_definitions    = file("./container_definitions.json")
+}
+
+resource "aws_ecs_service" "api_ecs" {
+  name                              = "api_ecs"
+  cluster                           = aws_ecs_cluster.api_ecs.arn
+  task_definition                   = aws_ecs_task_definition.api_ecs.arn
+  desired_count                     = 2
+  launch_type                       = "FARGATE"
+  platform_version                  = "1.3.0"
+  health_check_grace_period_seconds = 60
+
+  network_configuration {
+    assign_public_ip = false
+    security_groups  = [module.nginx_sg.security_group_id]
+
+    subnets = [
+      aws_subnet.private_0.id,
+      aws_subnet.private_1.id,
+    ]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.example.arn
+    container_name   = "example"
+    container_port   = 80
+  }
+
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+}
+
+
+# Modules
+
+# Security Group
+module "example_sg" {
+  source      = "./security_group"
+  name        = "module-sg"
+  vpc_id      = aws_vpc.rest_fastapi.id
+  port        = 80
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+
+# Nginx module
+module "nginx_sg" {
+  source      = "./security_group"
+  name        = "nginx-sg"
+  vpc_id      = aws_vpc.rest_fastapi
+  port        = 80
+  cidr_blocks = [aws_vpc.rest_fastapi.cidr_block]
+}
+
+
